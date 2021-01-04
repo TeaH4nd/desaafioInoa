73c5ee2068e6490e00ea1a914b8c58ace9523343
Alex Chamon <alexchamon@poli.ufrj.br>
2021-01-04 15:19:23 -0300

    Integracao com a api da hgbrasil

--- cotaAtivos/views.py
+++ cotaAtivos/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json 
 
+    if request.method == 'POST':
+        simbolo = request.POST['simbolo']
+
+        api_request = requests.get("https://api.hgbrasil.com/finance/stock_price?key=a5508924&symbol={}".format(simbolo))
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = 200
+
+        apiList = list(api['results'].values())
+        return render(request, 'home.html', {'api':apiList[0]})
+    else:
+        return render(request, 'home.html', {'simbolo':"Digite um simbolo acima para pesquisa"})
